2425ff94f05b51f412425e2f3c6f2413d981e14b
Gary Dong <garyd89@bu.edu>
2024-03-15 13:35:06 -0400

    added in SharedPreferences

--- app/src/main/java/com/example/randomimage/MainActivity.kt
+++ app/src/main/java/com/example/randomimage/MainActivity.kt
@@ -1,15 +1,20 @@
 package com.example.randomimage
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.util.Log
 import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.widget.doOnTextChanged
 import com.example.randomimage.databinding.ActivityMainBinding
+
 
 private const val TAG = "MainActivity"
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var sharedPreferences: SharedPreferences
+    private var currentImage = ""
+    private var currentText = ""
     private val images = arrayOf(
         R.drawable.pic_one,
         R.drawable.pic_two,
@@ -17,33 +22,78 @@ class MainActivity : AppCompatActivity() {
         R.drawable.pic_four,
         R.drawable.pic_five
     )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
 
-        //Wiring up the change button
+        // Initialize SharedPreferences
+        sharedPreferences = getSharedPreferences("RandomImagePreferences", MODE_PRIVATE)
+
+        // Restore state
+        restoreState()
+
+
+
+        // Wiring up the change button
         binding.changeButton.setOnClickListener() {
             Log.d(TAG, "Clicking Button")
             setRandomImage(binding.imageView)
         }
         // Initialize random image
-        setRandomImage(binding.imageView)
+        if (currentImage == ""){
+            setRandomImage(binding.imageView)
+        }
+
 
         //Save user's input
         binding.apply {
             userInput.doOnTextChanged() { text, _, _, _ ->
-                val input = text.toString()
-                Log.d("MainActivity", input)
+                currentText = text.toString()
+                Log.d("MainActivity", currentText)
             }
         }
+    }
+
+    override fun onDestroy() {
+        val editor = sharedPreferences.edit()
+        editor.putString("currentImage", currentImage)
+        editor.putString("currentText", currentText)
+        editor.commit()
+        Log.d(TAG, "onDestroy called")
+        super.onDestroy()
+
+
+
     }
 
     // Function to set a random image
     private fun setRandomImage(imageView: ImageView) {
         val randomIndex = (images.indices).random()
+        currentImage = images[randomIndex].toString()
         imageView.setImageResource(images[randomIndex])
+    }
+
+    private fun restoreState() {
+        Log.d(TAG, "Restoring state")
+        // Retrieve and set the stored image and text
+        val savedImage = sharedPreferences.getString("currentImage", "")
+        val savedText = sharedPreferences.getString("currentText", "")
+        if (savedImage!!.isNotEmpty()) {
+            Log.d(TAG, savedImage)
+            currentImage = savedImage
+            val resId = resources.getIdentifier(savedImage, "drawable", packageName)
+            if (resId != 0) { // Resource exists
+                binding.imageView.setImageResource(resId)
+            }
+        }
+        if (savedText!!.isNotEmpty()) {
+            Log.d(TAG, savedText)
+            currentText = savedText
+            binding.userInput.setText(savedText)
+        }
     }
 
 
